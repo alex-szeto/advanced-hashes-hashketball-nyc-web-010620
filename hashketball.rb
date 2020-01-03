f4b40900383b813b5c894c0c2df9cc31ad1cdadb
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 18:43:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -66,11 +66,11 @@ def game_hash
         :number => 0,
         :shoe => 16,
         :points => 22,
-        :rebounds => 12,
+        :rebounds => 12,,
         :assists => 12,
         :steals => 3,
         :blocks => 1,
-        :slam_dunks => 1,,
+        :slam_dunks => 1
         },
         {:player_name => "Reggie Evans",
         :number => 30,
