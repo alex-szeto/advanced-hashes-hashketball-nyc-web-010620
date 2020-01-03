691faa347c4aa0436132154b6b55636ff82bf693
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 18:13:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -146,4 +146,16 @@ def num_points_scored(players_name)
   end
 end
 
-
+def shoe_size(players_name)
+  game_hash.each do |place, team|
+    team.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if player[:player_name] == players_name
+            return player[:shoe]
+          end
+        end
+      end
+    end
+  end
+end
