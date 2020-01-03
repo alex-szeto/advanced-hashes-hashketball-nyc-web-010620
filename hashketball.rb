ab21bcb01cac10e291952f2dc72f461f2a8b9b96
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 17:43:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,7 +127,7 @@ def game_hash
   }
 end
 
-def num_points_scored(name)
+def num_points_scored(players_name)
   game_hash.each do |place, team|
     team.each do |attribute, data|
       if attribute == :players
