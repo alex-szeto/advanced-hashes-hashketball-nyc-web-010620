907875c1c485bc80c83a4ba97642f6dfccc23a28
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 18:05:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -26,9 +26,18 @@ def game_hash
         :blocks => 15,
         :slam_dunks => 10
         },
+        {:player_name => "Jeff Adrien",
+        :number => 4,
+        :shoe => 18,
+        :points => 10,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2
+        },
+        
         ]
-    
-      
     }
     
   }
